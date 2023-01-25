7c55b2908c78b586a21bfaa0050c261e673b2d41
alim <alimshussain@gmail.com>
2023-01-25 14:38:12 -0800

    migration file update

--- migrations/env.py
+++ migrations/env.py
@@ -3,7 +3,14 @@ from __future__ import with_statement
 import logging
 from logging.config import fileConfig
 
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from flask import current_app
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 from alembic import context
 
