9351f4210c24aa25c2f8ff17f2a3f22f646d3cc0
alim <alimshussain@gmail.com>
2023-02-02 14:16:45 -0800

    updates

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './loginform.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -18,6 +19,12 @@ const LoginForm = () => {
     }
   };
 
+  const demoLogin = () => {
+    setEmail('demo@aa.io')
+    setPassword('password')
+    return dispatch(login(email, password));
+  }
+
   const updateEmail = (e) => {
     setEmail(e.target.value);
   };
@@ -31,34 +38,44 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className='page-container-login'>
+    <div className='login-form-container'>
+      <form onSubmit={onLogin} className='all-form-login'>
+      <h1 className='vendough-logo-splash'>vendough</h1>
+      <p className='login-or-sign-message'>Log in or sign up</p>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div>
+          {/* <label htmlFor='email'>Email</label> */}
+          <input
+            className='form-box-login'
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div>
+          {/* <label htmlFor='password'>Password</label> */}
+          <input
+            className='form-box-login'
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+          <button className='sign-in-button' type='submit'>Sign in</button>
+          <button  className='demo-user-login' onClick={demoLogin} type='submit'>Demo User</button>
+          <NavLink className='sign-up-button-on-sign-in' to='/sign-up'>Sign Up</NavLink>
+      </form>
+    </div>
+    </div>
   );
 };
 
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -5,6 +5,8 @@ import { signUp } from '../../store/session';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
+  const [first_name, SetFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -14,17 +16,29 @@ const SignUpForm = () => {
 
   const onSignUp = async (e) => {
     e.preventDefault();
+    const errors = []
     if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+      const data = await dispatch(signUp(first_name, last_name, username, email, password));
       if (data) {
         setErrors(data)
       }
+    }else{
+      setErrors([...errors, 'Passwords do not match'])
+
     }
   };
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
   };
+
+  const updateFirstName = (e) => {
+    SetFirstName(e.target.value)
+  }
+
+  const updateLastName = (e) => {
+    setLastName(e.target.value)
+  }
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
@@ -39,55 +53,93 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to='/home' />;
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='page-container-login'>
+      <div className='login-form-container'>
+      <form onSubmit={onSignUp} className='all-form-login'>
+      <h1 className='vendough-logo-splash'>vendough</h1>
+      <p className='login-or-sign-message'>Sign Up</p>
+
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+
+        <div>
+          <input
+            className='form-box-login'
+            type='text'
+            name='First Name'
+            onChange={updateFirstName}
+            value={first_name}
+            required={true}
+            placeholder='First Name'
+          ></input>
+        </div>
+        <div>
+          <input
+            className='form-box-login'
+            type='text'
+            name='Last Name'
+            onChange={updateLastName}
+            value={last_name}
+            required={true}
+            placeholder='Last Name'
+          ></input>
+        </div>
+        <div>
+          <input
+            className='form-box-login'
+            type='text'
+            name='username'
+            onChange={updateUsername}
+            value={username}
+            required={true}
+            placeholder= 'Username'
+          ></input>
+        </div>
+        <div>
+          <input
+            className='form-box-login'
+            type='email'
+            name='email'
+            onChange={updateEmail}
+            value={email}
+            required={true}
+            placeholder= 'Email'
+          ></input>
+        </div>
+        <div>
+          <input
+            className='form-box-login'
+            type='password'
+            name='password'
+            onChange={updatePassword}
+            value={password}
+            required={true}
+            placeholder='Password'
+          ></input>
+        </div>
+        <div>
+          {/* <label>Repeat Password</label> */}
+          <input
+            className='form-box-login'
+            type='password'
+            name='repeat_password'
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+            placeholder='Repeat Password'
+          ></input>
+        </div>
+        <button className='sign-in-button' type='submit'>Sign Up</button>
+      </form>
       </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    </div>
   );
 };
 
